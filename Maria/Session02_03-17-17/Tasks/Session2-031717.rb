42b754c39fab3dd3ed73dc23516aa435d39033e7
Maria Alcocer <karolui>
2017-03-27 16:23:33 -0400

    Add folders to session-1-2

---
+++ Maria/Session02_03-17-17/Tasks/Session2-031717.rb
@@ -0,0 +1,11 @@
+puts 'Practice - Session 2'
+puts 'Convert seconds to minutes/gours/days'
+
+seconds = 5000.0
+toMinutes = seconds/60
+toHours = seconds/3600
+toDays = seconds/(3600 * 24)
+puts "5000 second is equal to #{toMinutes} minutes"
+puts "5000 second is equal to #{toHours} hours"
+puts "5000 second is equal to #{toDays} days"
+puts "THE END :D"
